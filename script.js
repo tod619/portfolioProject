053dba1ff5274c95cc27f7568dcf299813d79020
tod619 <trevorodwyer619@hotmail.com>
2021-01-26 14:44:11 +0000

    display progress in bars using js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const navbarOffSet = navbar.offsetTop;
 const sections = document.querySelectorAll('section')
 const navbarLinks = document.querySelectorAll('.navbar-link')
 
+const progress = document.querySelector('.progress-bars-wrapper')
+const progressBarsPercent = [97,90,82,75,80,70,55]
+
 
 window.addEventListener('scroll', () => {
     if(window.pageYOffset >= navbarOffSet){
@@ -22,4 +25,12 @@ window.addEventListener('scroll', () => {
             navbarLinks[i].classList.add('change');
         }
     })
+
+    if(window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll('.progress-percent').forEach((el,i) => {
+            el.style.width = `${progressBarsPercent[i]}%`
+
+            el.previousElementSibling.firstElementChild.textContent = progressBarsPercent[i];
+        })
+    }
 })
